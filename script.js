ec41e6951558eb0eb9e14a16a7c438ec468489e6
Felipe <carvalhofelipe.carvalho@gmail.com>
2022-01-26 19:30:08 +0000

    feat: final project completed

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    //Validation for "Principal" input box
+    if(principal <= 0) {
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    //Setting inner property of the result span tag
+    document.getElementById("result").innerHTML="If you deposit "+ "<mark>"+principal+"</mark>," + 
+    "<br/>at an interest rate of " + "<mark>"+rate+"%</mark>." +
+    "<br/>You will receive an amount of " + "<mark>"+amount+"</mark>" +
+    ",<br/>in the year " + "<mark>"+year+"</mark>";
 }
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+        
         
